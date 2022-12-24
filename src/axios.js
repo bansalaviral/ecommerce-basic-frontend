869aaabd1c3d0353e2bc4fb55eea542dbf010e8b
Aviral Bansal <nsitaviral@gmail.com>
2022-12-25 02:05:39 +0530

    fix

--- src/axios.js
+++ src/axios.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const token = JSON.parse(localStorage.getItem("user"))
-  ? JSON.parse(localStorage.getItem("user").token)
-  : "";
-
 const instance = axios.create({
   // baseURL: "www.google.com/",
   headers: {
     "Content-Type": "application/json",
     "Acess-Control-Allow-Origin": "*",
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${
+      localStorage.getItem("user")
+        ? JSON.parse(localStorage.getItem("user"))
+        : ""
+    }`,
     Accept: "application/json",
   },
 });
